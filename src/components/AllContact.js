16fb4232cef32f7a65530ec29e420a11190c33f4
mdsaj <mdsajjadhossain614@gmail.com>
2024-10-30 19:45:28 +0600

    new commit

--- src/components/AllContact.js
+++ src/components/AllContact.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import {Link} from "@mui/joy";
 import ContactCard from "./ContactCard";
+import {Col, Container, Row} from "reactstrap";
 
 export default function AllContacts() {
     return (
-        <div className="container mt-5">
-
-            {/*add button*/}
-
-
-            <div className="row">
-
-                {/*individual card detaild*/}
-                <div className="col-12 col-md-6 col-lg-4 mb-3">
-                    <div className="p-4 text-white">
-                        <Link href="/contact-details" style={{textDecoration: 'none'}}>
+        <Container className="mt-5">
+            <Row>
+                {[...Array(12)].map((_, index) => (
+                    <Col key={index} lg="3" md="4" sm="6" xs="12" className="mb-4">
+                        <Link href="/contact-details" style={{textDecoration: 'none',color:"black"}}>
                             <ContactCard
                                 image="https://scontent.fdac5-1.fna.fbcdn.net/v/t39.30808-6/459228707_2116824118713888_4717607413967779237_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=Gghhbe1KpkkQ7kNvgEYiYKY&_nc_zt=23&_nc_ht=scontent.fdac5-1.fna&_nc_gid=AkdMC1YakkN9cdsDRCEqLVv&oh=00_AYAPm8_F6iDClmMVWyqBzQZK4bKJUHWCAs5vzRPEtkwaKw&oe=672676D2"
                                 name="Md. Sajjad Hosaain"
@@ -24,11 +19,9 @@ export default function AllContacts() {
                                 relation="me"
                                 birthday="21, sept"/>
                         </Link>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-
+                    </Col>
+                ))}
+            </Row>
+        </Container>
     );
 }
--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -9,34 +9,24 @@ import CakeIcon from '@mui/icons-material/Cake';
 export default function ContactCard({image, name, email, phone, home, relation, birthday}) {
     return (
         <div style={{margin: 5}}>
-            <Card>
-                <div className="card-header">
-                    <Container className="my-4">
-                        <Row className="d-flex align-items-center">
-                            <Col md="6" className="p-3 bg-light">
-                                <img
-                                    style={{margin: 5}}
-                                    className="rounded-circle img-fluid profile-img"
-                                    alt="Profile"
-                                    src={image}
-                                    height="150px"
-                                    width="150px"
-                                />
-                            </Col>
-                            <Col align="left" md="6" className="p-3 bg-light">
-                                 {/*Right Component Content*/}
-                                <h5>{name}</h5>
-                                <p>{email}</p>
-                                <p>{phone}</p>
-                            </Col>
-                        </Row>
-                    </Container>
-                    <hr></hr>
-                    <div align="left">
-                        <p><HomeIcon/> {home}</p>
-                        <p><GroupIcon/> {relation}</p>
-                        <p><CakeIcon/> {birthday}</p>
-                    </div>
+            <Card body className="text-center">
+                <div align="center">
+                <img
+                    style={{margin: 5}}
+                    className="rounded-circle img-fluid profile-img"
+                    alt="Profile"
+                    src={image}
+                    height="100px"
+                    width="100px"/>
+                </div>
+                <h5>{name}</h5>
+                <p>{email}</p>
+                <p>{phone}</p>
+                <hr></hr>
+                <div align="left">
+                    <p><HomeIcon/> {home}</p>
+                    <p><GroupIcon/> {relation}</p>
+                    <p><CakeIcon/> {birthday}</p>
                 </div>
             </Card>
 
